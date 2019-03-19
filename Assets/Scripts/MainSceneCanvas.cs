11bff3c36aa1e7b9de0e7eaf662b2b58271d6791
OrloKun <orlocuario@gmail.com>
2019-03-18 21:52:03 -0300

    changes stages

---
+++ Assets/Scripts/MainSceneCanvas.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainSceneCanvas : MonoBehaviour {
+
+    private MainSceneController msController;
+    public GameObject[] availableStages;
+    public GameObject[] mainSceneObjects;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (FindObjectOfType<MainSceneController>())
+        {
+            msController = FindObjectOfType<MainSceneController>();
+        }
+        else
+        {
+            Debug.LogError("Te equivocaste loco!");
+        }
+
+        //Encontrar el Archivo del cual sacar las etapas
+        //Contar cuántas etapas son
+        //Calcular tamaño y cuadrícula de posición para crear cada uno de los objetos
+        //
+        //Considerar si es necesario un scrolling
+        //Generar los prefabs al tamaño indicado
+
+    }
+
+    public void ToggleCanvasObject(GameObject button, bool switchValue)
+    {
+        if (switchValue)
+        {
+            button.SetActive(true);
+        }
+        else
+        {
+            button.SetActive(false);
+        }
+    }
+
+    public void DeactivateStageSelectionCanvas()
+    {
+        foreach (GameObject gObject in availableStages)
+        {
+            ToggleCanvasObject(gObject, false);
+        }
+    }
+
+    public void ActivateStageSelectionCanvas()
+    {
+        foreach (GameObject gObject in availableStages)
+        {
+            ToggleCanvasObject(gObject, true);
+        }
+    }
+
+    public void ToggleMainSceneCanvas(bool switchStatus)
+    {
+        foreach (GameObject gObject in mainSceneObjects)
+        {
+            ToggleCanvasObject(gObject, switchStatus);
+        }
+    }
+
+}
---
+++ Assets/Scripts/MainSceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneController : MonoBehaviour
+{
+    MainSceneCanvas msCanvas;
+
+    private string userPlaying;
+    private int sceneToGo;
+
+
+    void Start()
+    {
+        msCanvas = FindObjectOfType<MainSceneCanvas>();
+        msCanvas.DeactivateStageSelectionCanvas();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void DisplayScenesAvailable()
+    {
+        msCanvas.ToggleMainSceneCanvas(false);
+        msCanvas.ActivateStageSelectionCanvas();
+    }
+
+
+
+}
